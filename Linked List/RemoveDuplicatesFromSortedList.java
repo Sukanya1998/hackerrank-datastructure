019e9ef248be55012790cff5d30eb3d56bc9b1e7
sukanya <sgvd.off@gmail.com>
2022-02-15 01:15:45 +0530

    Linked List part 2

---
+++ Linked List/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,128 @@
+package com.datastructures;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class RemoveDuplicatesFromSortedList {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            System.out.println(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                System.out.println(sep);
+            }
+        }
+    }
+
+
+    /*
+     * Complete the 'removeDuplicates' function below.
+     *
+     * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+     * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+     */
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+
+    public static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode head) {
+    	int data = head.data;
+    	if(head.next == null) return head;
+    	else{
+    		SinglyLinkedListNode temp = head;
+    		while(temp.next != null) {
+    			if(temp.next.data == data) {
+    				temp.next = temp.next.next;
+    				
+    			}else {
+    				temp = temp.next;
+    				data = temp.data;
+    			}
+    		}
+    		return head;
+    	}
+
+    }
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            SinglyLinkedListNode llist1 = removeDuplicates(llist.head);
+
+            printSinglyLinkedList(llist1, " ", null);
+            //bufferedWriter.newLine();
+        }
+
+        //bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
